6970ccffba45b5fcccd4d6b8fe4ad6193d5be282
Daniel Szymanek <github@e-danek.info>
2013-08-21 19:28:01 +0200

    A sprinkle of code comments

--- endomondo.inc.php
+++ endomondo.inc.php
@@ -63,32 +63,31 @@ class Endomondo {
 		93 => 'Climbing',
 		94 => 'Treadmill walking'
 	);
-	function __construct($config) {
+	function __construct($config) { //Sets up basic enviorment for our class to work in.
 		$this->config=$config;
 		$this->ua_string="Dalvik/1.6.0 (Linux; U; ". $config['os'] ." ". $config['os_version'] ."; ". $config['model'] ." Build/GRI40)";
 		$this->getAuthToken();
 	}
-	private function defeatPagination($url, $params, $limit, $before=null)
-	{
-		if($limit<1) return (object) array('data' => array()); //Nothin do do here
+	private function defeatPagination($url, $params, $limit, $before=null) { //Fetches multiple pages of data, so that we can have our data as a whole.
+		if($limit<1) return array(); //Nothin do do here
 		if($before['time']) $params['before'] = $before['time']; //See if were going in retrospect
-		if($before['id']) $params['beforeId'] = $before['id']; 
+		if($before['id']) $params['beforeId'] = $before['id']; //ID needed for activities (aside from time)
 		$response = json_decode($this->makeRequest($url,"GET",$params));
 		$current_count=count($response->data);
-		if(is_array($before) && isset($response->more) && !$response->data[0]) return (object) array('data' => array()); //No more results (reached the end)
+		if(is_array($before) && isset($response->more) && !$response->data[0]) return array(); //No more results (reached the end)
 		elseif($current_count<$limit) { //Limit is not sustained. We need more entries.
-			if(isset($response->data[0]->start_time)) {
+			if(isset($response->data[0]->start_time)) { //Are we operating on workouts? (or is it activities)
 				$before = array('time' => end($response->data)->start_time);
-			} else {
+			} else { //In case of activities
 				$last=end($response->data);
 				$before = array('time' => $last->order_time, 'id' => $last->id);
 			}
 			$more_results = $this->defeatPagination($url, $params, $limit-$current_count, $before);
-			return (object) array('data' => array_merge($response->data, $more_results->data));
+			return array_merge($response->data, $more_results);
 		}
-		return $response; //If we got what we needed, just return fetched data
+		return $response->data; //If we got what we needed, just return fetched data
 	}
-	private function makeRequest($url, $method=null, $params=null) {
+	private function makeRequest($url, $method=null, $params=null) { //Helper method for making requests
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_USERAGENT, $this->ua_string);
@@ -109,7 +108,7 @@ class Endomondo {
 			return false;
 		} else {
 			curl_close($ch);
-			switch ($params['compression']) {
+			switch ($params['compression']) { //Handle compressed data, acc. to compression we chose.
 				case 'gzip':
 					return gzinflate(substr($response, 10));
 					break;
@@ -122,7 +121,7 @@ class Endomondo {
 			}		
 		}
 	}
-	private function requestAuthToken() {
+	private function requestAuthToken() { //Grabs authentication token after signing in with provided credentials
 		$params = array(
 			'email' =>			$this->config['email'],
 			'password' =>		$this->config['password'],
@@ -145,11 +144,11 @@ class Endomondo {
 			return false;
 		}
 	}
-	private function getAuthToken() {
+	private function getAuthToken() { //Passes authentication token over, or requests one.
 		if($this->config['auth_token']!=null) return $this->config['auth_token'];
 		return $this->config['auth_token'] = $this->requestAuthToken();
 	}
-	function getActivities($user=null, $limit=15) {
+	function getActivities($user=null, $limit=15) { //Fetches a list of activities (Feed). Presents only data relevant to single user if user's id is provided.
 		$url=$this->config['endomondo_host']."/mobile/api/feed";
 		$params = array(
 			'authToken' =>	$this->getAuthToken(),
@@ -160,14 +159,14 @@ class Endomondo {
 		if($user!=null) $params['userId'] = $user;
 		return $this->defeatPagination($url, $params, $limit);
 	}
-	function getFriendsSummary() {
+	function getFriendsSummary() { //TODO: Fetches a list of friends with timestamps of their latest activity. Useful for checking if cached data is up-to-date.
 		$url=$this->config['endomondo_host']."/mobile/friends";
 		$params = array(
 			'authToken' =>	$this->getAuthToken(),
 			'language' =>		'pl'
 		);
 	}
-	function getWorkoutList($user=null, $limit=20) {
+	function getWorkoutList($user=null, $limit=20) { //Fetches a list of workouts, either your own or specified user's
 		$url=$this->config['endomondo_host']."/mobile/api/workouts";
 		$params = array(
 			'authToken' =>	$this->getAuthToken(),
@@ -179,7 +178,7 @@ class Endomondo {
 		if($user!=null) $params['userId'] = $user;
 		return $this->defeatPagination($url, $params, $limit);
 	}
-	function getWorkoutDetails($id) {
+	function getWorkoutDetails($id) { //Fetches details of a single workout inc. track points.
 		$url=$this->config['endomondo_host']."/mobile/api/workout/get";
 		$params = array(
 			'authToken' =>	$this->getAuthToken(),
--- index.php
+++ index.php
@@ -2,7 +2,11 @@
 require_once 'config.inc.php';
 require_once 'endomondo.inc.php';
 $endo = new Endomondo($config);
-print_r ($endo->getMyWorkouts());
-
+// print_r (count($endo->getActivities(null, 15))); //'11684995'
+// echo" + ";
+// print_r (count($endo->getWorkoutList(null, 24))); 
+foreach ($endo->getActivities(null, 31)->data as $value) {
+	echo $value->id.": ".$value->order_time.": ".$value->from->name." - ".$value->message->text."\n";
+}
 //echo print_r($endo->getWorkoutDetails(233188289));
 ?>
