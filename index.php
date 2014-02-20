7106767682fa29afdc1e1c3612c54d3d7fd23105
Daniel Szymanek <github@e-danek.info>
2014-02-20 07:40:18 +0100

    Sucha mess, don't even look here...

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 require_once 'config.inc.php';
 require_once 'endomondo.inc.php';
 $endo = new Endomondo($config);
-// print_r (count($endo->getActivities(null, 15))); //'11684995'
+// print_r (count($endo->getActivities(null, 15))); //''
 // echo" + ";
 // print_r (count($endo->getWorkoutList(null, 24))); 
 // foreach ($endo->getActivities(null, 31)->data as $value) {
 // 	echo $value->id.": ".$value->order_time.": ".$value->from->name." - ".$value->message->text."\n";
 // }
-echo print_r($endo->getWorkoutDetails(236789518));//233188289));
+echo print_r($endo->getWorkoutDetails());//));
 //$fr=$endo->getFriendsSummary();
 //print_r($fr);
 ?>
